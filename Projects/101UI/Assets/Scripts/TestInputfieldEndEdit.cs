f662569292061d150d669ea6ff8ac79b06b9c927
Manic <zhz@hongliu.cc>
2018-09-16 21:02:26 +0800

    InputField

---
+++ Projects/101UI/Assets/Scripts/TestInputfieldEndEdit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestInputfieldEndEdit : MonoBehaviour
+{
+    InputField _inputField;
+    void Start()
+    {
+        _inputField = GetComponent<InputField>();
+
+        _inputField.onEndEdit.AddListener(OnEndEdit);
+        _inputField.onEndEdit.AddListener(delegate (string text) { Debug.Log(text); });
+        _inputField.onEndEdit.AddListener(text => { Debug.Log(text); });
+    }
+
+    private void OnEndEdit(string text)
+    {
+        Debug.Log(text);
+    }
+}
---
+++ Projects/101UI/Assets/Scripts/TestInputfieldValueChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestInputfieldValueChange : MonoBehaviour
+{
+    InputField _inputField;
+    void Start()
+    {
+        _inputField = GetComponent<InputField>();
+
+        _inputField.onValueChanged.AddListener(OnValueChage);
+
+        _inputField.onValueChanged.AddListener(OnValueChage);
+        _inputField.onValueChanged.AddListener(delegate (string text) { Debug.Log(text); });
+        _inputField.onValueChanged.AddListener(text => { Debug.Log(text); });
+    }
+
+    private void OnValueChage(string text)
+    {
+        Debug.Log(text);
+    }
+}
