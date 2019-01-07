1de5ff151cdb48999dcec237af11b8dc0f70190a
Manic <zhz@hongliu.cc>
2019-01-07 19:09:52 +0800

    粒子系统更新

---
+++ Projects/187ParticleUpdates/Assets/Scripts/ParticleAPI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAPI : MonoBehaviour
+{
+    ParticleSystem particle;
+
+    void Start()
+    {
+        ParticleSystem.Particle[] array = new ParticleSystem.Particle[1000];
+        particle.GetParticles(array, 1000, 500);
+
+        particle.SetParticles(array, 1000, 1000);
+    }
+}
