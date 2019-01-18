8f11537043ef0ef8cdda9925d9ab96f44bb9a086
Manic <zhz@hongliu.cc>
2019-01-18 15:36:12 +0800

    191

---
+++ Projects/191WriteReadFile/Assets/Scripts/LearnFile.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class LearnFile : MonoBehaviour
+{
+    void Start()
+    {
+        var str = File.ReadAllText("text.txt");
+        Debug.Log(str);
+    }
+}
