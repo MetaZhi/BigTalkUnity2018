621698386022096742f27f1cc880072d882a6442
Manic <zhz@hongliu.cc>
2019-01-18 15:34:24 +0800

    188VisualEffectGraph

---
+++ Projects/188VisualEffectGraph/Assets/VFXEventSender.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VFXEventSender : MonoBehaviour
+{
+    void Start()
+    {
+        var com = GetComponent<VisualEffect>();
+        com.SendEvent("OnPlay");
+    }
+}
