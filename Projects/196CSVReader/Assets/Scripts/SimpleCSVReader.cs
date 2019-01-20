3d247a5acdd9b810f14a6a0c03e05ac3b6992e39
Manic <zhz@hongliu.cc>
2019-01-20 19:33:11 +0800

    196

---
+++ Projects/196CSVReader/Assets/Scripts/SimpleCSVReader.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class SimpleCSVReader : MonoBehaviour
+{
+    void Start()
+    {
+        var path = Path.Combine(Application.streamingAssetsPath, "data.csv");
+        // 按行读取
+        var csvStr = File.ReadAllLines(path);
+        foreach (var item in csvStr)
+        {
+            var values = item.Split(',');
+        }
+    }
+}
---
+++ Projects/196CSVReader/Assets/Scripts/UsingCsvHelper.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class Npc
+{
+    public int Id { get; set; }
+    public string 名称 { get; set; }
+    public float 血量 { get; set; }
+    public float 攻击力 { get; set; }
+    public float 防御力 { get; set; }
+}
+
+public class UsingCsvHelper : MonoBehaviour
+{
+    void Start()
+    {
+        var path = Path.Combine(Application.streamingAssetsPath, "data.csv");
+        var csvStr = File.ReadAllText(path);
+
+        using (var reader = new StringReader(csvStr))
+        using (var csv = new CsvReader(reader))
+        {
+            // 需要using System.Linq;才能使用ToList()
+            var records = csv.GetRecords<Npc>().ToList();
+            Debug.Log(records.Count);
+        }
+    }
+}
