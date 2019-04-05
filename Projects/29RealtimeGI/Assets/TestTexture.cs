373cf690c26027b17003f9ea51a7a3f1a64c4843
Manic <zhz@hongliu.cc>
2019-04-05 18:24:52 +0800

    add 29

---
+++ Projects/29RealtimeGI/Assets/TestTexture.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTexture : MonoBehaviour
+{
+    public Texture tex;
+
+	// Use this for initialization
+    void Start()
+    {
+        var child = transform.GetChild(0);
+        child.GetComponent<Renderer>().material.mainTexture = tex;
+
+        
+    }
+
+    void Update()
+    {
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            var mmm = child.gameObject.GetComponent<Renderer>();
+
+            if (i == 0)
+            {
+                mmm.material.mainTexture = tex;
+
+            }
+
+            i++;
+        }
+    }
+}
