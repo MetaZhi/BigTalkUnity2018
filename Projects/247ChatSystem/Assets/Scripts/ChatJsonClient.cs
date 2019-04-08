d0be46bf288fe4f66731b5b4b968ca6ee76ca22e
Manic <zhz@hongliu.cc>
2019-04-08 18:14:51 +0800

    247 封包解包

--- Projects/247ChatSystem/Assets/Scripts/ChatJsonClient.cs
+++ Projects/247ChatSystem/Assets/Scripts/ChatJsonClient.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EchoClient : MonoBehaviour
+class ChatMesssage
+{
+    public string Nickname;
+    public string Msg;
+}
+
+public class ChatJsonClient : MonoBehaviour
 {
     public InputField Input;
     public InputField Nickname;
@@ -15,8 +22,11 @@ public class EchoClient : MonoBehaviour
     private Socket socket;
     private byte[] buffer = new byte[1024];
 
+    // 用来存储接收到的数据
+    List<byte> DataCache = new List<byte>();
+
     // 缓存接收到的新消息
-    private List<string> Messages = new List<string>();
+    private List<ChatMesssage> Messages = new List<ChatMesssage>();
 
     void Start()
     {
@@ -36,7 +46,7 @@ public class EchoClient : MonoBehaviour
         {
             foreach (var str in Messages)
             {
-                ReceivedText.text = ReceivedText.text + str + "\n";
+                ReceivedText.text = ReceivedText.text + str.Nickname + " 说：" + str.Msg + "\n";
             }
             Messages.Clear(); //清除处理过的消息
         }
@@ -51,9 +61,8 @@ public class EchoClient : MonoBehaviour
             int length = socket.EndReceive(ar);
             if (length > 0)
             {
-                var str = Encoding.UTF8.GetString(buffer, 0, length);
-                Debug.Log($"接收到服务端的消息:{str}");
-                Messages.Add(str);
+                DataCache.AddRange(buffer.Take(length));
+                ProcessData();
 
                 // 重新开始接收
                 socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
@@ -70,6 +79,21 @@ public class EchoClient : MonoBehaviour
         }
     }
 
+    private void ProcessData()
+    {
+        while (true)
+        {
+            var json = Decode();
+            if (string.IsNullOrEmpty(json))
+                break;
+
+            var chatMsg = JsonUtility.FromJson<ChatMesssage>(json);
+
+            Debug.Log($"接收到服务端的消息:{chatMsg.Msg}");
+            Messages.Add(chatMsg);
+        }
+    }
+
     private void OnServerDisconnect()
     {
         Debug.Log("与服务端断开连接");
@@ -80,12 +104,50 @@ public class EchoClient : MonoBehaviour
     // 点击按钮后调用Send方法
     public void Send()
     {
-        string msg = Nickname.text + "|" + Input.text;
+        var chatMsg = new ChatMesssage(){
+            Nickname = Nickname.text,
+            Msg = Input.text
+        };
+
+        byte[] msg = Encode(JsonUtility.ToJson(chatMsg));
         // 3. 发送数据
         for (int i = 0; i < 10; i++)
         {
-            socket.Send(Encoding.UTF8.GetBytes(msg));
+            socket.Send(msg);
         }
+    }
+
+    // 封包
+    byte[] Encode(string json)
+    {
+
+        byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+
+        var length = BitConverter.GetBytes((ushort)jsonBytes.Length);
+        byte[] bytes = new byte[jsonBytes.Length + 2];
+
+        Buffer.BlockCopy(length, 0, bytes, 0, 2);
+        Buffer.BlockCopy(jsonBytes, 0, bytes, 2, jsonBytes.Length);
+
+        return bytes;
+    }
+
+    // 解包
+    string Decode()
+    {
+        string json = null;
+        if (DataCache.Count < 2) return null;
+
+        var length = BitConverter.ToUInt16(DataCache.Take(2).ToArray(), 0);
+        // 判断数据是否足够，如果不够可能原因是发生分包，下次再解析
+        if (DataCache.Count - 2 >= length)
+        {
+            var bytes = DataCache.Take(length + 2).ToArray();
+            json = Encoding.UTF8.GetString(bytes, 2, length);
+            DataCache.RemoveRange(0, length + 2);
+        }
+
+        return json;
     }
 
     void OnDestroy()
